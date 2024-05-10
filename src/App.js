e1563ca9d437bd7caf6df15165bdcc3981098bbf
Darien Labbe <darienlabbe@gmail.com>
2024-05-10 12:43:12 -0700

    added basic nav bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
-import './App.css';
 
 function App() {
   return (
     <div className="">
-      <header className="App-header">
-        
+      <header className="">
+        <div>
+          <div>
+            <a>Darien Labbe</a>
+          </div>
+          <div>
+            <ul>
+              <li>Home</li>
+              <li>Projects</li>
+              <li>Skills</li>
+              <li>About</li>
+              <li>Contact</li>
+            </ul>
+          </div>
+        </div>
       </header>
     </div>
   );
